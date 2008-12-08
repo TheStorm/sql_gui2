269c2f09579986b1ac2ebb8f4dab646510a250c6
Sebastian Danninger <checker2100@gmx.de>
2008-12-08 01:31:16 +0100

    lol

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -71,7 +71,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(899, 569);
+            this.tabPage1.Size = new System.Drawing.Size(911, 569);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Connect";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -81,9 +81,9 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(899, 569);
+            this.tabPage2.Size = new System.Drawing.Size(911, 569);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Commands";
+            this.tabPage2.Text = "Statements";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
